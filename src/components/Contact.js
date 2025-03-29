0d26150d0b6e76c9a8be6adcb2cdc3e751934ff1
chandu <marthasaichandu@gmail.com>
2025-03-29 19:54:57 +0530

    final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,8 @@ const Contact = () => {
         <section className="contact">
             <h2>Contact Me</h2>
             <p>Email: marthasaichandu267@gmail.com</p>
-            <p>LinkedIn: <a href="www.linkedin.com/in/martha-sai-chandu">Your Profile</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/martha-sai-chandu/">Your Profile</a></p>
+            <p>phone no : 7013782360</p>
         </section>
     );
 };
